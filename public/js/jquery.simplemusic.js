8f96d5ab2b22143b264b12e6685641b6a7192071
songxin <504252262@qq.com>
2016-06-19 18:58:41 +0800

    sm summary porker

--- public/js/jquery.simplemusic.js
+++ public/js/jquery.simplemusic.js
@@ -5,40 +5,55 @@
 	var _playstatus = false;
 	var _palyInterval;
 	var n=0;
+	var urls = [
+				"/public/mus/Sunrise.mp3",
+				"/public/mus/Lost.mp3",
+				"/public/mus/ElectricRemo.mp3",
+				"/public/mus/PerfectCircle.mp3",
+				"/public/mus/ELDorado.mp3",
+				"/public/mus/ViolinConcerto.mp3"	
+		];
+
 	$.fn.simplemusic=function(settings){
 		$(this).css({"z-index":"999","width":"44px","height":"44px","position":"fixed","bottom":"10px","right":"10px","cursor":"pointer","background":"url(http://7xnlfe.com1.z0.glb.clouddn.com/o_music_note_big.png)"}).bind("click",function(){
-			var musLen = settings.length;
-			var rand = parseInt(Math.random()*musLen+1)
+			
 			if(_playstatus){
-				pause(this,rand);
+				pause(this);
 			}else{
-				play(this,rand);
+				play(this);
 			}
 		});
 		var audio = document.createElement("audio");
 		$(audio).attr("id","audioPlayer").css("display","none").prependTo(this);
 		
 		if(settings.loop && settings.loop==true){ $(audio).attr("loop","loop");}
-		if(settings.urls && settings.urls instanceof Array){
-			for(var i in settings.urls){
-				var source = document.createElement("source");
-				$(source).attr("src",settings.urls[i]).appendTo($(audio));
-			}
+		
+		if(urls && urls instanceof Array){
+			var musLen = urls.length - 1;
+			var musId = parseInt(Math.random()*musLen);
+			var source = document.createElement("source");
+			$(source).attr("src",urls[musId]).appendTo($(audio));
 		}
+		//当一首歌结束时，
+		//$("#audio").attr("onended","javascript:alert('This is a test!');");
+
 		if(settings.autoplay&&settings.autoplay == true){$(this).click();}
 	};
-	function play(e,index){
+
+	function play(e){
 		_palyInterval = setInterval(function(){
 			startRotate(e);
 		},10);
-		$("#audioPlayer")[index].play();
+		$("#audioPlayer")[0].play();
 		_playstatus=true;
 	}
-	function pause(e,index){
-		$("#audioPlayer")[index].pause();
+
+	function pause(e){
+		$("#audioPlayer")[0].pause();
 		clearInterval(_palyInterval);
-		_playstatus=false;
+		_playstatus=false;	
 	}
+
 	function startRotate(e){
 		n=n+1;
 		e.style.transform="rotate(" + n + "deg)";
@@ -47,6 +62,6 @@
 		e.style.MozTransform="rotate(" + n + "deg)";
 		e.style.opacity = Math.abs(n/360-0.5)+0.5+"";
 		if (n==360){n=0}
-		
 	}
+
 })(jQuery);
