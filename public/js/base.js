7bdd38a5fda8a8d3d355841be1a8c8a46228c746
e <xin.song@cimc.com>
2017-05-06 18:09:21 +0800

    modify fancy js

--- public/js/base.js
+++ public/js/base.js
@@ -77,36 +77,6 @@ $(document).ready(function() {
   });
   contentEffects();
 
- /* /!* Init Fancybox *!/
-  $('.fancybox').fancybox({
-	padding : 0,
-	openEffect  : 'elastic',
-	closeBtn: false
-
-  });
-
-  $('.fancybox-buttons').fancybox({
-	openEffect  : 'none',
-	closeEffect : 'none',
-
-	prevEffect : 'none',
-	nextEffect : 'none',
-
-	closeBtn  : false,
-
-	helpers : {
-		title : {
-			type : 'inside'
-		},
-		buttons	: {}
-	},
-
-	afterLoad : function() {
-		/!*this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');*!/
-		/!*this.title = this.title ? this.title : ' ';*!/
-	}
-	});*/
-
 
 });
 function contentEffects(){
